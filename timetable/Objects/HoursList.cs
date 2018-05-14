592640dedb59dd62f41e794ed7737ca33176e0df
Karim Osman <karimosman@192.168.1.12>
2018-05-14 14:57:33 +0200

    Created Hourslist (not finished)

---
+++ timetable/Objects/HoursList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Timetable.timetable.DB;
+
+namespace Timetable.timetable.Objects
+{
+	public class HoursList : AbstractList
+    {
+		public HoursList(DataModel _db)  : base(_db)
+        {
+			SetListElement("Hours_List");
+        }
+
+		public override void Create()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
