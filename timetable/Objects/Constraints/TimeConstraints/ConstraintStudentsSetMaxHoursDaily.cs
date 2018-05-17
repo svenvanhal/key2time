2abbcabee4647bd3c212bb5f55738853b158f2ac
Karim Osman <karimosman@192.168.1.12>
2018-05-17 11:40:47 +0200

    Added Test XML creator

--- timetable/Objects/Constraints/TimeConstraints/ConstraintStudentsSetMaxHoursDaily.cs
+++ timetable/Objects/Constraints/TimeConstraints/ConstraintStudentsSetMaxHoursDaily.cs
@@ -38,7 +38,7 @@ namespace Timetable.timetable.Objects
 		}
 
 		public override XElement[] Create(DataModel dB)
-		{
+		{ //TO DO: Check query
 			var query = from g in dB.tt_GradeLesson
 						join l in dB.School_Lookup_Grade on g.gradeId equals l.GradeID
 						select new { g.numberOfLessons, l.GradeName };
