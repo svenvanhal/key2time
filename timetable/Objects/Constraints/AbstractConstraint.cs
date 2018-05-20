bb10cda645cf4781987ecfd7059fcb1accfa83de
Karim Osman <karimosman@192.168.1.12>
2018-05-20 10:15:00 +0200

    Fix in abstractlist, removed redundent check

--- timetable/Objects/Constraints/AbstractConstraint.cs
+++ timetable/Objects/Constraints/AbstractConstraint.cs
@@ -9,44 +9,29 @@ namespace Timetable.timetable.Objects
 	{
 		public int weight { get; set; }
 		public XElement constraint { get; set; }
-              
-        /// <summary>
-        /// Sets the weight.
-        /// </summary>
-        /// <param name="w">The width.</param>
+
+		/// <summary>
+		/// Sets the weight.
+		/// </summary>
+		/// <param name="w">The width.</param>
 		public void SetWeight(int w)
 		{
 			weight = w;
-			if (constraint != null)
-			{
-				var el = constraint.Element("Weight_Percentage");
-				if (el == null)
-				{
+			constraint.Add(new XElement("Weight_Percentage", weight));
 
-					constraint.Add(new XElement("Weight_Percentage", weight));
-
-				}
-				else{
-					el.SetValue(w);
-				}
-			}
-			else
-			{
-				Console.WriteLine("[Error] no element set");
-			}
 		}
-        /// <summary>
-        /// Sets the element.
-        /// </summary>
-        /// <param name="s">S.</param>
+		/// <summary>
+		/// Sets the element.
+		/// </summary>
+		/// <param name="s">S.</param>
 		public void SetElement(string s)
 		{
 			constraint = new XElement(s);
 		}
-        /// <summary>
-        /// Returns the XElement
-        /// </summary>
-        /// <returns>The xelement.</returns>
+		/// <summary>
+		/// Returns the XElement
+		/// </summary>
+		/// <returns>The xelement.</returns>
 		public abstract XElement ToXelement();
 		public abstract XElement[] Create(DataModel dB);
 	}
