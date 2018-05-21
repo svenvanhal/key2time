049663b03d01f6403b328f75603c3138243dbe2f
Sven van Hal <sven@svenvanhal.nl>
2018-05-21 11:24:51 +0200

    Fix additional merge conflicts.

--- timetable/Application.cs
+++ timetable/Application.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
 using Timetabling.Algorithms;
+using Timetabling.DB;
+using Timetabling.Objects;
+using Timetabling.XML;
 
 namespace Timetabling
 {
@@ -30,6 +33,49 @@ namespace Timetabling
             Console.Read();
 #endif
         }
+
+        public static void Main2()
+        {
+            var dB = new DataModel();
+            var xmlCreator = XmlCreator.Instance;
+
+            var daysList = new DaysList(dB);
+            daysList.Create();
+
+            var hoursList = new HoursList(dB);
+            hoursList.Create();
+
+            var teachersList = new TeachersList(dB);
+            teachersList.Create();
+
+            var subjectsList = new SubjectsList(dB);
+            subjectsList.Create();
+
+            var activitiesList = new ActivitiesList(dB);
+            activitiesList.Create();
+
+            var yearsList = new YearsList(dB);
+            yearsList.Create();
+
+            var timeConstraintsList = new TimeConstraintsList(dB);
+            timeConstraintsList.Create();
+
+            var spaceConstraintsList = new SpaceConstraintsList(dB);
+            spaceConstraintsList.Create();
+
+            xmlCreator.AddToRoot(daysList.GetList());
+            xmlCreator.AddToRoot(hoursList.GetList());
+            xmlCreator.AddToRoot(teachersList.GetList());
+            xmlCreator.AddToRoot(yearsList.GetList());
+            xmlCreator.AddToRoot(subjectsList.GetList());
+            xmlCreator.AddToRoot(activitiesList.GetList());
+            xmlCreator.AddToRoot(timeConstraintsList.GetList());
+            xmlCreator.AddToRoot(spaceConstraintsList.GetList());
+
+            xmlCreator.Save();
+
+        }
+
 
     }
 }
