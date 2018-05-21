994d0e358180c1cc9d49a91f718dafc811eb1f1d
Sven van Hal <sven@svenvanhal.nl>
2018-05-21 10:36:58 +0200

    Change path where FET file is generated for compatibility, minor fixes.

--- timetable/Application/Main.cs
+++ timetable/Application/Main.cs
@@ -8,46 +8,45 @@ namespace Timetable
 {
     public class Application
     {
-		public static void Main()
+        public static void Main()
         {
             var dB = new DataModel();
-            XmlCreator xmlCreator = XmlCreator.Instance;
+            var xmlCreator = XmlCreator.Instance;
 
-			var daysList = new DaysList(dB);
+            var daysList = new DaysList(dB);
             daysList.Create();
-            
-			var hoursList = new HoursList(dB);
-			hoursList.Create();
 
-			var teachersList = new TeachersList(dB);
-			teachersList.Create();
-            
-			var subjectsList = new SubjectsList(dB);
-			subjectsList.Create();
+            var hoursList = new HoursList(dB);
+            hoursList.Create();
 
-			var activitiesList = new ActivitiesList(dB);
-			activitiesList.Create();
+            var teachersList = new TeachersList(dB);
+            teachersList.Create();
 
-			var yearsList = new YearsList(dB);
-			yearsList.Create();
-            
-			var timeConstraintsList = new TimeConstraintsList(dB);
-			timeConstraintsList.Create();
+            var subjectsList = new SubjectsList(dB);
+            subjectsList.Create();
 
-			var spaceConstraintsList = new SpaceConstraintsList(dB);
-			spaceConstraintsList.Create();
-            
-			xmlCreator.AddToRoot(daysList.GetList());
-			xmlCreator.AddToRoot(hoursList.GetList());
-			xmlCreator.AddToRoot(teachersList.GetList());
-			xmlCreator.AddToRoot(yearsList.GetList());
-			xmlCreator.AddToRoot(subjectsList.GetList());
-			xmlCreator.AddToRoot(activitiesList.GetList());
-			xmlCreator.AddToRoot(timeConstraintsList.GetList());
-			xmlCreator.AddToRoot(spaceConstraintsList.GetList());
+            var activitiesList = new ActivitiesList(dB);
+            activitiesList.Create();
 
+            var yearsList = new YearsList(dB);
+            yearsList.Create();
 
-			xmlCreator.Save();
+            var timeConstraintsList = new TimeConstraintsList(dB);
+            timeConstraintsList.Create();
+
+            var spaceConstraintsList = new SpaceConstraintsList(dB);
+            spaceConstraintsList.Create();
+
+            xmlCreator.AddToRoot(daysList.GetList());
+            xmlCreator.AddToRoot(hoursList.GetList());
+            xmlCreator.AddToRoot(teachersList.GetList());
+            xmlCreator.AddToRoot(yearsList.GetList());
+            xmlCreator.AddToRoot(subjectsList.GetList());
+            xmlCreator.AddToRoot(activitiesList.GetList());
+            xmlCreator.AddToRoot(timeConstraintsList.GetList());
+            xmlCreator.AddToRoot(spaceConstraintsList.GetList());
+
+            xmlCreator.Save();
 
         }
 
