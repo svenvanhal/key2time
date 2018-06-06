06da6fd65c217db2c97ce939576e9c1695c2a217
Karim Osman <karimosman1@hotmail.com>
2018-06-06 23:29:44 +0200

    Update DataModel.cs

--- Timetabling/DB/DataModel.cs
+++ Timetabling/DB/DataModel.cs
@@ -182,12 +182,6 @@ namespace Timetabling.DB
 					.WithRequired(e => e.Subject_MasterData_Subject)
 					.WillCascadeOnDelete(false);
 
-			modelBuilder.Entity<Tt_Class>()
-					.HasMany(e => e.Tt_ClassGroup)
-					.WithRequired(e => e.Tt_Class)
-					.HasForeignKey(e => e.classId)
-					.WillCascadeOnDelete(false);
-
 			modelBuilder.Entity<TeacherClassSubjectGroup>()
 					.Property(e => e.GroupId)
 					.IsFixedLength();
